1962d64bf736948aed2c110eed7ff455d18efde2
EliAlarcon <EliAlarcon@users.noreply.github.com>
2024-06-17 00:44:04 -0500

    Hook control estado alfabeto

--- src/screens/play/components/WordCardComponent.tsx
+++ src/screens/play/components/WordCardComponent.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { Button, Card, Text } from "react-native-paper";
 import { styles } from "../../../theme/styles";
 
 export const WordCardComponent = () => {
+
+    //Hook para controlar el estado del botón presionado del alfabeto
+    const [pressLetter, setPressLetter] = useState<string>("");
+
+    //Función para almacenar la letra presionada
+    
+
   const displayAlphabet = () => {
     //Arreglo para las letras del alfabeto
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
